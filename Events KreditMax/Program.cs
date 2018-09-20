cd5cbaf5c5d3f2d77666c4f2b7fe4af53bc23d2a
Henrik <_henrik_breer@hotmail.com>
2018-09-20 14:44:46 +0200

    .

---
+++ Events KreditMax/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_KreditMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kunde k = new Kunde() { KreditMax = -500 };
+            k.KreditOverskredet += (s, e) => { Console.WriteLine("Kredit overskredet "); };
+
+            k.Køb(100);
+            k.Køb(500);
+
+            if (System.Diagnostics.Debugger.IsAttached)
+
+            {
+
+                Console.Write("Press any key to continue . . . ");
+
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+
+    public class Kunde
+    {
+        // event mangler
+        public string Navn { get; set; }
+        public int Saldo { get; set; }
+        public int KreditMax { get; set; }
+        public void Køb(int værdi)
+        {
+            Console.WriteLine("Kunde {0} køber for {1}", Navn, værdi);
+            this.Saldo -= værdi;
+            if (Saldo < KreditMax)
+            {
+                if (KreditOverskredet != null) // kald til event mangler
+                    KreditOverskredet(this, new EventArgs());
+            }
+        }
+
+        public event EventHandler KreditOverskredet;
+        
+                
+    }
+
+
+
+}
---
+++ Events/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.IO.FileSystemWatcher f = new System.IO.FileSystemWatcher(@"c:\temp\");
+            f.EnableRaisingEvents = true;
+            f.Created += (s, e) => { Console.WriteLine("Oprettet" + e.FullPath); };
+            f.Deleted += (s,e) => { Console.WriteLine("Slettet" + e.FullPath); };
+            f.Changed += (s, e) => { Console.WriteLine("Ændret" + e.FullPath); };
+            
+            f.Changed += (s, e) => {
+                
+                Console.WriteLine(System.IO.File.ReadAllText(e.FullPath)); };
+            
+            do
+            {
+
+            } while (true);
+        }
+    }
+}
