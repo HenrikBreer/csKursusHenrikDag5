503f3127f181c1ebcfac31109ee2f2cf5305eb9f
Henrik <_henrik_breer@hotmail.com>
2018-09-20 15:50:20 +0200

    .

--- Using Oprydning/Program.cs
+++ Using Oprydning/Program.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Microsoft.SqlServer.Server;
 
 namespace Using_Oprydning
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
 
+            
             using (StreamReader stream = System.IO.File.OpenText(@"c:\temp\dyrenavne.txt"))
             { 
 
