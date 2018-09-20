7d3cf1915da502711d4ea83350f1e05bba1de13b
Henrik <_henrik_breer@hotmail.com>
2018-09-20 15:29:06 +0200

    .

---
+++ Using Oprydning/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Using_Oprydning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader stream = System.IO.File.OpenText(@"c:\temp\dyrenavne.txt"))
+            { 
+
+                while (stream.Peek() != -1)
+                {
+                    string navn = stream.ReadLine();
+                    Console.WriteLine(navn);
+                }
+            stream.Close();
+            //stream = null;
+
+            Console.ReadKey();
+            }
+        }
+                     
+    }
+
+}
